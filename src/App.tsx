0c19c9b638fc91d25901e62a1474376355346c2f
Masud Rana <masudrana58855@gmail.com>
2023-10-02 11:00:54 +0600

    test tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -10,7 +7,7 @@ function App() {
       <div>
         <h1>The Last Chapter</h1>
       </div>
-      <p className="read-the-docs">
+      <p className="text-3xl font-bold underline">
         This is a book online book store
       </p>
     </>
